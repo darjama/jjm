937847d3fc961ba935e964f57a57773f47c31d7d
Daryl Marco <darmarco@gmail.com>
2020-06-09 21:50:27 -0700

    Added contact page content

--- jjm/src/pages/contact-me.js
+++ jjm/src/pages/contact-me.js
@@ -1,16 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
+import {Button} from "react-bootstrap"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout pageInfo={{ pageName: "page-2" }}>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+const ContactMe = () => (
+  <Layout pageInfo={{ pageName: "contact-me" }}>
+    <SEO title="Contact Therapist Jennifer Marco" />
+    <h1>Contact Me</h1>
+    <p><br/></p>
+    <Button variant="primary" size="lg" block
+      onClick={() => window.open("mailto:info@jennifermarco.com")}>
+      info{`@`}jennifermarco.com
+    </Button>
+    <Button variant="secondary" size="lg" block
+      onClick={() => window.open("tel:+16096539900")}>
+      (609) 653-9900
+    </Button>
+    <p><br/></p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default ContactMe
